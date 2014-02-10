3fa78bfee85026c13032af472a6468e9fc0fac76
Alexandre Kandalintsev <spam@messir.net>
2014-02-10 10:41:33 +0000

    do.sh: save results to specified prefix

--- do.sh
+++ do.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-make
+PREFIX=$1
+  if [ -z "$PREFIX" ]; then
+  echo "please specify prefix where to save data" 1>&2
+  exit 1
+fi
+echo "saving results to $PREFIX" 1>&2
+mkdir -p $PREFIX
+
+make LPROF=1
 killall -q experiment
 
 sleep 0.1
-taskset -ac 0 ./experiment > ./results/single
+taskset -ac 0 ./experiment > $PREFIX/single
 sleep 0.1
 taskset -ac 1 ./experiment polute &
 sleep 0.1
-taskset -ac 0 ./experiment > ./results/double
+taskset -ac 0 ./experiment > $PREFIX/double
 sleep 0.1
-taskset -ac 0 ./experiment `pidof experiment` > ./results/double_with_stop
+taskset -ac 0 ./experiment `pidof experiment` > $PREFIX/double_with_stop
 
 killall experiment
